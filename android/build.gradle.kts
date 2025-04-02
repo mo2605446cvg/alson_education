cd7707f7dde09b353e8c0b24bca013d385bb696c
mo2605446cvg <mo8457ed@gmail.com>
2025-04-02 05:15:40 +0200

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,6 +2,9 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.0")
@@ -13,6 +16,9 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
 }
 
