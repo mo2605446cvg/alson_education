074701f054f8872e6595171d3fafd5fa269073f4
mo2605446cvg <mo8457ed@gmail.com>
2025-04-02 05:14:31 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -3,10 +3,14 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
     plugins {
         id("com.android.application") version "8.1.0"
         id("org.jetbrains.kotlin.android") version "1.9.0"
+        id("dev.flutter.flutter-gradle-plugin") version "2.0.0"
     }
 }
 
