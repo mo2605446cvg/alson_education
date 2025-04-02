3f0c8918d881277ea23725033eb3caebc70b43e4
mo2605446cvg <mo8457ed@gmail.com>
2025-04-02 04:44:25 +0200

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,10 +3,10 @@ buildscript {
         google()
         mavenCentral()
     }
-    // في android/build.gradle:
-dependencies {
-    classpath 'com.android.tools.build:gradle:8.1.0' // <-- التحديث هنا
-}
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
 }
 
 allprojects {
@@ -16,15 +16,6 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
