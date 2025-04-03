a34b15650dee6b1899ddab2f02246a723fa604ed
mo2605446cvg <mo8457ed@gmail.com>
2025-04-04 00:35:26 +0200

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String databaseName = 'alson_education.db';
+  static const String defaultAdminCode = 'admin123';
+  static const String defaultAdminPassword = 'adminpass';
+}
