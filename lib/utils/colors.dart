f374defa197d3eb0020ff4bbb4ecb96487652121
mo2605446cvg <mo8457ed@gmail.com>
2025-04-04 13:10:24 +0200

    Update colors.dart

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Colors.blue;
-  static const Color secondaryColor = Colors.grey;
-  static const Color accentColor = Colors.amber;
-  static const Color textColor = Colors.black87;
+  static const Color primaryColor = Color(0xFF0288D1); // أزرق فاتح مريح
+  static const Color secondaryColor = Color(0xFFF5F5F5); // رمادي فاتح للخلفية
+  static const Color accentColor = Color(0xFFFFA726); // برتقالي دافئ للتأكيد
+  static const Color textColor = Color(0xFF212121); // أسود غامق للنصوص
+  static const Color errorColor = Color(0xFFD32F2F); // أحمر للأخطاء
 }
