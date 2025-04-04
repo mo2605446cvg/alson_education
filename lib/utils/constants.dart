d877ff579b30c8542c79bbe249d6aa9f447782c9
mo2605446cvg <mo8457ed@gmail.com>
2025-04-04 12:33:27 +0200

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,4 +2,6 @@ class AppConstants {
   static const String databaseName = 'alson_education.db';
   static const String defaultAdminCode = 'admin123';
   static const String defaultAdminPassword = 'adminpass';
+  static const String defaultAdminUsername = 'Admin'; // إضافة الثابت
+  static const String defaultAdminDepartment = 'إدارة'; // إضافة الثابت
 }
