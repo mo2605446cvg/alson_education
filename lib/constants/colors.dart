b26386e7182258440b0a00b7830c32218e67d7e2
mo2605446cvg <mo8457ed@gmail.com>
2025-04-06 21:57:55 +0200

    Delete lib directory

--- lib/constants/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color PRIMARY_COLOR = Colors.blue;
-const Color SECONDARY_COLOR = Colors.grey;
-const Color ACCENT_COLOR = Colors.amber;
-const Color TEXT_COLOR = Colors.black87;
--- lib/utils/helpers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showSnackBar(BuildContext context, String message, {Color color = Colors.red}) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message, style: TextStyle(color: color))));
-}
