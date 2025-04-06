26558321d4a70cb87193f8dc5c951ea825e16f7a
mo2605446cvg <mo8457ed@gmail.com>
2025-04-06 21:29:21 +0200

    Delete lib directory

--- lib/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String databaseName = 'alson_education.db';
-  static const String defaultAdminCode = 'admin123';
-  static const String defaultAdminPassword = 'adminpass';
-  static const String defaultAdminUsername = 'Admin'; // إضافة الثابت
-  static const String defaultAdminDepartment = 'إدارة'; // إضافة الثابت
-}
