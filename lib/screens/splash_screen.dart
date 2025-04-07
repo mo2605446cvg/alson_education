837e858b785e3a49d11e0c0c2400f61b56653033
mo2605446cvg <mo8457ed@gmail.com>
2025-04-07 21:30:38 +0200

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -5,26 +5,34 @@ import 'package:alson_education/screens/onboarding_screen.dart';
 import 'package:alson_education/screens/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     final appState = Provider.of<AppState>(context);
 
-    Future.delayed(Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => appState.currentUserCode == null ? OnboardingScreen() : HomeScreen()),
+        MaterialPageRoute(
+          builder: (context) => appState.currentUserCode == null ? const OnboardingScreen() : const HomeScreen(),
+        ),
       );
     });
 
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/img/icon.png', width: 150),
-            SizedBox(height: 20),
-            CircularProgressIndicator(),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/img/icon.png', width: 150),
+              const SizedBox(height: 20),
+              const CircularProgressIndicator(),
+            ],
+          ),
         ),
       ),
     );
