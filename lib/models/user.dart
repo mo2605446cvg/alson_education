2aab75f7e8165dbd568faff88ceddbb5bf0fdc4e
mo2605446cvg <mo8457ed@gmail.com>
2025-04-07 11:25:59 +0200

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,22 +1,8 @@
-import 'package:hive/hive.dart';
-
-part 'user.g.dart';
-
-@HiveType(typeId: 0)
 class User {
-  @HiveField(0)
   final String code;
-
-  @HiveField(1)
   final String username;
-
-  @HiveField(2)
   final String department;
-
-  @HiveField(3)
   final String role;
-
-  @HiveField(4)
   final String password;
 
   User({
@@ -26,4 +12,24 @@ class User {
     required this.role,
     required this.password,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'code': code,
+      'username': username,
+      'department': department,
+      'role': role,
+      'password': password,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      code: map['code'] as String,
+      username: map['username'] as String,
+      department: map['department'] as String,
+      role: map['role'] as String,
+      password: map['password'] as String,
+    );
+  }
 }
