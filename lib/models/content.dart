132f7ae3490763ca4afdfe2b6c8f6df209c8983a
mo2605446cvg <mo8457ed@gmail.com>
2025-04-07 11:26:38 +0200

    Update content.dart

--- lib/models/content.dart
+++ lib/models/content.dart
@@ -1,25 +1,9 @@
-import 'package:hive/hive.dart';
-
-part 'content.g.dart';
-
-@HiveType(typeId: 1)
 class Content {
-  @HiveField(0)
   final String? id;
-
-  @HiveField(1)
   final String title;
-
-  @HiveField(2)
   final String filePath;
-
-  @HiveField(3)
   final String fileType;
-
-  @HiveField(4)
   final String uploadedBy;
-
-  @HiveField(5)
   final String uploadDate;
 
   Content({
@@ -30,4 +14,26 @@ class Content {
     required this.uploadedBy,
     required this.uploadDate,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'file_path': filePath,
+      'file_type': fileType,
+      'uploaded_by': uploadedBy,
+      'upload_date': uploadDate,
+    };
+  }
+
+  factory Content.fromMap(Map<String, dynamic> map) {
+    return Content(
+      id: map['id'] as String?,
+      title: map['title'] as String,
+      filePath: map['file_path'] as String,
+      fileType: map['file_type'] as String,
+      uploadedBy: map['uploaded_by'] as String,
+      uploadDate: map['upload_date'] as String,
+    );
+  }
 }
