891e24e0fe4c466796374e529cc1bf6bd7a6cd53
mo2605446cvg <mo8457ed@gmail.com>
2025-04-09 01:19:09 +0200

    Update upload_content_screen.dart

--- lib/screens/upload_content_screen.dart
+++ lib/screens/upload_content_screen.dart
@@ -6,7 +6,7 @@ import 'package:alson_education/providers/app_state_provider.dart';
 import 'package:alson_education/services/database_service.dart';
 import 'package:alson_education/services/storage_service.dart';
 import 'package:alson_education/models/content.dart';
-import 'package:alson_education/constants/strings.dart';
+import 'package:alson_education/constants/app_strings.dart';
 
 class UploadContentScreen extends StatefulWidget {
   const UploadContentScreen({super.key});
