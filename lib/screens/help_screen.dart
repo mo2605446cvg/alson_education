fc695dda719a70b57aa9f27140fdeaa3124a7178
mo2605446cvg <mo8457ed@gmail.com>
2025-04-09 01:09:16 +0200

    Update help_screen.dart

--- lib/screens/help_screen.dart
+++ lib/screens/help_screen.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:alson_education/providers/app_state_provider.dart';
 import 'package:alson_education/constants/strings.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HelpScreen extends StatelessWidget {
   const HelpScreen({super.key});
+
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +39,14 @@ class HelpScreen extends StatelessWidget {
                   textAlign: TextAlign.center,
                 ),
                 const SizedBox(height: 20),
-                const Text('Email: m.nasrmm2002@gmail.com', textAlign: TextAlign.center),
-                const Text('Phone: 01023828155', textAlign: TextAlign.center),
+                InkWell(
+                  onTap: () => _launchURL('mailto:m.nasrmm2002@gmail.com'),
+                  child: const Text('Email: m.nasrmm2002@gmail.com', textAlign: TextAlign.center),
+                ),
+                InkWell(
+                  onTap: () => _launchURL('tel:01023828155'),
+                  child: const Text('Phone: 01023828155', textAlign: TextAlign.center),
+                ),
               ],
             ),
           ),
