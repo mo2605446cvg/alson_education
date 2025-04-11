13bce0df23be652063a1e7d32bde79a2ee5491f9
mo2605446cvg <mo8457ed@gmail.com>
2025-04-11 15:59:08 +0200

    Update help_screen.dart

--- lib/screens/help_screen.dart
+++ lib/screens/help_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:alson_education/providers/app_state_provider.dart';
-import 'package:alson_education/constants/app_strings.dart';
+import 'package:alson_education/constants/strings.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class HelpScreen extends StatelessWidget {
