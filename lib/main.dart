731a58981e7f3e1f8fdc0259b6b2f84d241363a5
mo2605446cvg <mo8457ed@gmail.com>
2025-04-17 21:40:14 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,16 @@ import 'package:alson_education/screens/splash_screen.dart';
 import 'package:alson_education/screens/home_screen.dart';
 import 'package:alson_education/screens/profile_screen.dart';
 import 'package:alson_education/screens/admin_users_screen.dart';
+import 'package:alson_education/screens/admin_dashboard_screen.dart';
 import 'package:alson_education/screens/upload_content_screen.dart';
 import 'package:alson_education/screens/content_screen.dart';
 import 'package:alson_education/screens/chat_screen.dart';
 import 'package:alson_education/screens/results_query_screen.dart';
 import 'package:alson_education/screens/view_results_screen.dart';
 import 'package:alson_education/screens/help_screen.dart';
+import 'package:alson_education/screens/login_screen.dart';
+import 'package:alson_education/screens/register_screen.dart';
+import 'package:alson_education/screens/onboarding_screen.dart';
 import 'package:alson_education/providers/app_state_provider.dart';
 import 'package:alson_education/providers/theme_provider.dart';
 
@@ -37,12 +41,16 @@ class AlsonEducation extends StatelessWidget {
               '/home': (context) => const HomeScreen(),
               '/profile': (context) => const ProfileScreen(),
               '/admin/users': (context) => const AdminUsersScreen(),
+              '/admin/dashboard': (context) => const AdminDashboardScreen(),
               '/admin/upload': (context) => const UploadContentScreen(),
               '/content': (context) => const ContentScreen(),
               '/chat': (context) => const ChatScreen(),
               '/results': (context) => const ResultsQueryScreen(),
               '/view_results': (context) => ViewResultsScreen(),
               '/help': (context) => const HelpScreen(),
+              '/login': (context) => const LoginScreen(),
+              '/register': (context) => const RegisterScreen(),
+              '/onboarding': (context) => const OnboardingScreen(),
               '/error': (context) => const ErrorScreen(errorMessage: 'An error occurred'),
             },
             initialRoute: '/splash',
