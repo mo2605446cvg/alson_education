356f578e3e5f277e5cae0c34a461045fc7b95d33
mo2605446cvg <mo8457ed@gmail.com>
2025-04-17 21:53:02 +0200

    Update app_state_provider.dart

--- lib/providers/app_state_provider.dart
+++ lib/providers/app_state_provider.dart
@@ -5,6 +5,7 @@ class AppState with ChangeNotifier {
   String? _currentUserCode;
   String? _currentUserRole;
   String? _currentUserDepartment;
+  String? _currentUserDivision;
   bool _isLoading = false;
   String _language = 'ar';
   bool _hasError = false;
@@ -14,17 +15,19 @@ class AppState with ChangeNotifier {
   String? get currentUserCode => _currentUserCode;
   String? get currentUserRole => _currentUserRole;
   String? get currentUserDepartment => _currentUserDepartment;
+  String? get currentUserDivision => _currentUserDivision;
   bool get isLoading => _isLoading;
   bool get isAdmin => _currentUserRole == 'admin';
   String get language => _language;
   bool get hasError => _hasError;
   String? get errorMessage => _errorMessage;
 
-  void login(String email, String code, String role, String department) {
+  void login(String email, String code, String role, String department, {String division = ''}) {
     _currentUserEmail = email;
     _currentUserCode = code;
     _currentUserRole = role;
     _currentUserDepartment = department;
+    _currentUserDivision = division;
     _hasError = false;
     _errorMessage = null;
     notifyListeners();
@@ -35,6 +38,7 @@ class AppState with ChangeNotifier {
     _currentUserCode = null;
     _currentUserRole = null;
     _currentUserDepartment = null;
+    _currentUserDivision = null;
     _hasError = false;
     _errorMessage = null;
     notifyListeners();
