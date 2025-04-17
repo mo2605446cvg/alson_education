7cd19bee4e79dd1e311ef6c30b0a6a6d166d50e0
mo2605446cvg <mo8457ed@gmail.com>
2025-04-17 21:54:35 +0200

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,16 +2,25 @@ class User {
   final String code;
   final String username;
   final String department;
+  final String division;
   final String role;
   final String password;
 
-  User({required this.code, required this.username, required this.department, required this.role, required this.password});
+  User({
+    required this.code,
+    required this.username,
+    required this.department,
+    required this.division,
+    required this.role,
+    required this.password,
+  });
 
   Map<String, dynamic> toMap() {
     return {
       'code': code,
       'username': username,
       'department': department,
+      'division': division,
       'role': role,
       'password': password,
     };
@@ -22,6 +31,7 @@ class User {
       code: map['code'] as String,
       username: map['username'] as String,
       department: map['department'] as String,
+      division: map['division'] as String? ?? '',
       role: map['role'] as String,
       password: map['password'] as String,
     );
