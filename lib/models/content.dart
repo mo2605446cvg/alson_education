803fdb18828d6024cab375f927f0c9497b18e7ab
mo2605446cvg <mo8457ed@gmail.com>
2025-04-17 21:54:14 +0200

    Update content.dart

--- lib/models/content.dart
+++ lib/models/content.dart
@@ -2,17 +2,23 @@ class Content {
   final String? id;
   final String title;
   final String filePath;
+  final String? posterPath;
   final String fileType;
   final String uploadedBy;
   final String uploadDate;
+  final String department;
+  final String division;
 
   Content({
     this.id,
     required this.title,
     required this.filePath,
+    this.posterPath,
     required this.fileType,
     required this.uploadedBy,
     required this.uploadDate,
+    required this.department,
+    required this.division,
   });
 
   Map<String, dynamic> toMap() {
@@ -20,9 +26,12 @@ class Content {
       'id': id,
       'title': title,
       'file_path': filePath,
+      'poster_path': posterPath,
       'file_type': fileType,
       'uploaded_by': uploadedBy,
       'upload_date': uploadDate,
+      'department': department,
+      'division': division,
     };
   }
 
@@ -31,9 +40,12 @@ class Content {
       id: map['id'] as String?,
       title: map['title'] as String? ?? '',
       filePath: map['file_path'] as String? ?? '',
+      posterPath: map['poster_path'] as String?,
       fileType: map['file_type'] as String? ?? '',
       uploadedBy: map['uploaded_by'] as String? ?? '',
       uploadDate: map['upload_date'] as String? ?? '',
+      department: map['department'] as String? ?? '',
+      division: map['division'] as String? ?? '',
     );
   }
 }
