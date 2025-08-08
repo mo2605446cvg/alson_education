7abf51cf94a04dbbb22036364215795a9f1dfdff
mo2605446cvg <mo8457ed@gmail.com>
2025-08-08 10:59:20 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,16 +19,14 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.alson_education"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
+   defaultConfig {
+    applicationId = "com.example.alson_education"
+    minSdk = 21
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0.0"
+}
+
 
     buildTypes {
         release {
@@ -42,3 +40,4 @@ android {
 flutter {
     source = "../.."
 }
+
