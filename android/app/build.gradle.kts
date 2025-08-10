14ea53d21e0423023e6110d244805f0dd7e8f21b
mo2605446cvg <mo8457ed@gmail.com>
2025-08-10 05:20:52 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = "com.example.alson_education"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0.0"
     }
