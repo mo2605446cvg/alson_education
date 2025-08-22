68f008111644035315dd67c3d3b88b555cb7f4a2
mo2605446cvg <mo8457ed@gmail.com>
2025-08-22 18:28:47 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
   // ...
-  id("com.android.application") version "8.1.0" apply false
-  id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 
   // Add the dependency for the Google services Gradle plugin
   id("com.google.gms.google-services") version "4.4.3" apply false
@@ -28,4 +26,5 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
 
